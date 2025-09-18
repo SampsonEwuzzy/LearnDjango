e25d2c72f2c4f3dbe493884e9ec9b317e7545768
Sampson Ikechukwu Ewuzie <sampson.ewuzie@abujaelectricity.com>
2025-09-18 14:28:24 +0100

    added posts

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import HomePageView
 from .views import AboutPageView
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls')),
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
